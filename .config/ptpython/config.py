9d35b6f4aa5d947c06cae87ab5c48109e2a036a4
Gabriel Linder <linder.gabriel@gmail.com>
2020-11-29 00:36:25 +0100

    This time should be the one.
    Signed-off-by: Gabriel Linder <linder.gabriel@gmail.com>

--- .config/ptpython/config.py
+++ .config/ptpython/config.py
@@ -1,6 +1,6 @@
 # Copy this file to ~/.config/ptpython/config.py
 
-from __future__ import unicode_literals
+from prompt_toolkit.output import ColorDepth
 from ptpython.layout import CompletionVisualisation
 
 
@@ -16,20 +16,22 @@ def configure(repl):
     repl.highlight_matching_parenthesis = True
     repl.wrap_lines = True
     repl.enable_mouse_support = False
-    repl.complete_while_typing = True
-    repl.enable_fuzzy_completion = True
+    # XXX complete_while_typing is conflicting with enable_history_search
+    repl.complete_while_typing = False
+    repl.enable_fuzzy_completion = False
     repl.enable_dictionary_completion = False
     repl.vi_mode = False
     repl.paste_mode = False
     repl.prompt_style = 'classic'
     repl.insert_blank_line_after_output = False
-    repl.enable_history_search = False
+    # XXX enable_history_search is conflicting with complete_while_typing
+    repl.enable_history_search = True
     repl.enable_auto_suggest = True
     repl.enable_open_in_editor = False
     repl.enable_system_bindings = False
     repl.confirm_exit = False
     repl.enable_input_validation = True
-    repl.color_depth = 'DEPTH_24_BIT'
+    repl.color_depth = ColorDepth.DEPTH_24_BIT
     repl.enable_syntax_highlighting = True
     repl.vi_start_in_navigation_mode = False
     repl.vi_keep_last_used_mode = False
