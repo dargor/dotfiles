92cac0de6b68fde27a987b25a93813e1337d4376
Gabriel Linder <linder.gabriel@gmail.com>
2024-12-18 09:04:30 +0100

    Sync with upstream.
    Signed-off-by: Gabriel Linder <linder.gabriel@gmail.com>

--- .vim/py/roles.py
+++ .vim/py/roles.py
@@ -7,13 +7,7 @@ if "PYTEST_VERSION" in os.environ:
 roles_py_imported = True
 
 def load_ai_role_names(command_type):
-    roles_config_path = os.path.expanduser(vim.eval("g:vim_ai_roles_config_file"))
-    if not os.path.exists(roles_config_path):
-        raise Exception(f"Role config file does not exist: {roles_config_path}")
-
-    roles = configparser.ConfigParser()
-    roles.read(roles_config_path)
-
+    roles = read_role_files()
     enhance_roles_with_custom_function(roles)
 
     role_names = set()
@@ -21,5 +15,7 @@ def load_ai_role_names(command_type):
         parts = name.split('.')
         if len(parts) == 1 or parts[-1] == command_type:
             role_names.add(parts[0])
+
+    role_names = [name for name in role_names if name != DEFAULT_ROLE_NAME]
 
     return list(role_names)
